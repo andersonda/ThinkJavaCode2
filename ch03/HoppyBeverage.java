b273c85bd0c35a1885c8a0216cdf8eefe930bcb9
David Anderson <andersonda8109@gmail.com>
2019-06-26 16:59:30 -0700

    hoppy beverage example

--- ch03/HoppyBeverage.java
+++ ch03/HoppyBeverage.java
@@ -1,23 +1,80 @@
-// David Anderson
+/* Hoppy Beverage Problem
+
+  **************************
+  Recipe:
+  **************************
+  15 oz of hops
+  12.5 lbs of malt
+  1 packet of yeast
+
+  Recipe brews 5 gallons
+    of "Hoppy Beverage"
+
+  **************************
+  Prices:
+  **************************
+  2 oz pouch of hops: $3.99
+  malt: $1.50 per lb
+  yeast: $2.59 per pouch
+
+*/
 public class HoppyBeverage {
 
+  public static final int GALLONS_PER_BATCH = 5;
+
   public static void main(String[] args){
+    System.out.println("Testing pouchesOfHops...");
+    System.out.println(pouchesOfHops(1));
+    System.out.println(pouchesOfHops(5));
+    System.out.println(pouchesOfHops(10));
+    System.out.println();
+
+    System.out.println("Testing poundsOfMalt...");
+    System.out.println(poundsOfMalt(1));
+    System.out.println(poundsOfMalt(5));
+    System.out.println(poundsOfMalt(10));
+    System.out.println();
+
+    System.out.println("Testing packetsOfYeast...");
+    System.out.println(packetsOfYeast(1));
+    System.out.println(packetsOfYeast(5));
+    System.out.println(packetsOfYeast(10));
+    System.out.println();
+
+
+    System.out.println("Testing totalCost...");
+    double myGallons = 5;
+    System.out.println("$" + totalCost(5));
+    System.out.println("$" + totalCost(7));
+    System.out.println("$" + totalCost(10));
+    System.out.println("$" + totalCost(100));
 
   }
 
+  // given the desired number of gallons, returns the required number of hops pouches
   public static int pouchesOfHops(double gallons){
-    return 0;
+    int ozPerBatch = 15;
+    int ozPerPouch = 2;
+    return (int) Math.ceil(gallons * ozPerBatch / GALLONS_PER_BATCH / ozPerPouch);
   }
 
+  // given the desired number of gallons, returns the required pounds of malt
   public static double poundsOfMalt(double gallons){
-    return 0.0;
+    double lbsPerBatch = 12.5;
+    return gallons * lbsPerBatch / GALLONS_PER_BATCH;
   }
 
+  // given the desired number of gallons, returns the required packets of yeast
   public static int packetsOfYeast(double gallons){
-    return 0;
+    return (int) Math.ceil(gallons / GALLONS_PER_BATCH);
   }
 
+  // given the desired number of gallons, returns the required pouches of yeast
   public static double totalCost(double gallons){
-    return 0.0;
+    double pricePerPouchOfHops = 3.99;
+    double pricePerPoundOfMalt = 1.50;
+    double pricePerPacketOfYeast = 2.59;
+
+    return pricePerPouchOfHops * pouchesOfHops(gallons) + pricePerPoundOfMalt * poundsOfMalt(gallons) + pricePerPacketOfYeast * packetsOfYeast(gallons);
   }
 }
