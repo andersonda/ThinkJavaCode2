801e02f2a7d6902ab2ce09c34a1cb05fed79bf47
David Anderson <andersonda8109@gmail.com>
2019-06-26 15:18:23 -0700

    added skeleton for hoppy beverage problem

---
+++ ch03/HoppyBeverage.java
@@ -0,0 +1,23 @@
+// David Anderson
+public class HoppyBeverage {
+
+  public static void main(String[] args){
+
+  }
+
+  public static int pouchesOfHops(double gallons){
+    return 0;
+  }
+
+  public static double poundsOfMalt(double gallons){
+    return 0.0;
+  }
+
+  public static int packetsOfYeast(double gallons){
+    return 0;
+  }
+
+  public static double totalCost(double gallons){
+    return 0.0;
+  }
+}
