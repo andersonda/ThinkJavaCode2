2adea05f11aaf4b5565819f73ca0e97d521525f2
David Anderson <andersonda8109@gmail.com>
2019-07-10 20:11:24 -0700

    added additional array examples

---
+++ ch06/ArraysCS142.java
@@ -0,0 +1,40 @@
+public class ArraysCS142{
+  public static void main(String[] args){
+    int[] someInts = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    System.out.println("Length of the array: " + someInts.length);
+    System.out.println("First element: " + someInts[0]);
+    System.out.println("Second element: " + someInts[1]);
+    System.out.println("Last element: " + someInts[someInts.length - 1]);
+    System.out.println("Sum of the elements in the array: " + sum(someInts));
+    System.out.println("The largest element of the array: " + max(someInts));
+    System.out.println("The smallest element of the array: " + min(someInts));
+  }
+
+  public static int sum(int[] array){
+    int sum = 0;
+    for(int i = 0; i < array.length; i++){
+      sum += array[i];
+    }
+    return sum;
+  }
+
+  public static int max(int[] array){
+    int largestSoFar = array[0];
+    for(int i = 1; i < array.length; i++){
+      if(array[i] > largestSoFar){
+        largestSoFar = array[i];
+      }
+    }
+    return largestSoFar;
+  }
+
+  public static int min(int[] array){
+    int smallestSoFar = array[0];
+    for(int i = 1; i < array.length; i++){
+      if(array[i] < smallestSoFar){
+        smallestSoFar = array[i];
+      }
+    }
+    return smallestSoFar;
+  }
+}
