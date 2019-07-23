0f9b5f8a476c459339f4af91e4c700f4949f3729
David Anderson <andersonda8109@gmail.com>
2019-07-22 18:28:40 -0700

    refactoring for readability

--- ch10/References.java
+++ ch10/References.java
@@ -1,15 +1,16 @@
 public class References{
   public static void main(String[] args){
-    int[] x = {3};
-    int[] y = x; // Copy x's reference value into y
     int a = 3;
     int b = a; // Copy a's primitive value into b
-    x[0] = 4;
     a = 4;
-    System.out.println(x[0]); // Prints 4
-    System.out.println(y[0]); // Also prints 4 (same object as x)
     System.out.println(a); // Prints 4
     System.out.println(b); // Prints 3 (b held a copy of a's value)
+    
+    int[] x = {3};
+    int[] y = x; // Copy x's reference value into y
+    x[0] = 4;
+    System.out.println(x[0]); // Prints 4
+    System.out.println(y[0]); // Also prints 4 (same object as x)
 
     // prints out the reference value for x and y
     System.out.println(x);
